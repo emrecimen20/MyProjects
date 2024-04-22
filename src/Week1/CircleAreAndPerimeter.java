3544ec56b3ecb9099be1775686d840c15ff45421
emrecimen20 <emrecimen48@outlook.com>
2024-04-23 03:01:48 +0845

    Added a program that calculates the area of a circle with known center angle and radius

---
+++ src/Week1/CircleAreAndPerimeter.java
@@ -0,0 +1,15 @@
+package Week1;
+import java.util.Scanner;
+
+public class CircleAreAndPerimeter {
+    public static void main(String[] args) {
+        double pi=3.14; int r,merkezAci;
+        Scanner input= new Scanner(System.in);
+        System.out.print("Yarıçap Giriniz :");
+        r=input.nextInt();
+        System.out.print("Merkez Açı Giriniz :");
+        merkezAci=input.nextInt();
+        double alan=(pi*(r*r)*merkezAci)/360;
+        System.out.print("Üçgenin Alanı :" +alan);
+    }
+}
