fd7ede4935867e9a6c674de0c16f9a1c70164bdd
emrecimen20 <emrecimen48@outlook.com>
2024-04-26 00:00:52 +0845

    Chinese zodiac program added

---
+++ src/Week1/ChineseZodiac.java
@@ -0,0 +1,62 @@
+/* Çin Zodyağı nedir?
+
+4000 bin yıldır kullanılan bir astroloji çeşididir Çin astrolojisi ve insanları 12 değişik burç ve sembollerle tanımlar.
+ Çin Zodyağı bu 12 burcun eşit aralıklarla(10 derece genişliğinde) sıralandığı bir hayvan halkasıdır ve yıldızlarla pek bir ilgisi yoktur.
+Çin Zodyağı nasıl hesaplanır?
+Çin zodyağı hesaplanırken kişinin doğum yılının 12 ile bölümünde kalana göre bulunur.*/
+
+package Week1;
+
+import java.util.Scanner;
+
+public class ChineseZodiac {
+    public static void main(String[] args) {
+       int birthYear;   //Doğum yılı değişkenimiz.
+        Scanner input=new Scanner(System.in);
+
+        //Kullanıcıdan doğum yılını girmesi için değişkeni scannerla tanımladık.
+        System.out.print("Doğum yılınızı giriniz :");
+        birthYear=input.nextInt();
+
+        //Doğum yılının 12 ye bölümünden kalan sayıya göre seneryolar oluşturduk.Bunun için switch-case yapısını kullandık.
+        switch ((birthYear)%12){
+            case 0:
+                System.out.println("Çin Zodyağı Burcunuz : Maymun");
+                break;
+            case 1:
+                System.out.println("Çin Zodyağı Burcunuz : Horoz");
+                break;
+            case 2:
+                System.out.println("Çin Zodyağı Burcunuz : Köpek");
+                break;
+            case 3:
+                System.out.println("Çin Zodyağı Burcunuz : Domuz");
+                break;
+            case 4:
+                System.out.println("Çin Zodyağı Burcunuz : Fare");
+                break;
+            case 5:
+                System.out.println("Çin Zodyağı Burcunuz : Öküz");
+                break;
+            case 6:
+                System.out.println("Çin Zodyağı Burcunuz : Kaplan");
+                break;
+            case 7:
+                System.out.println("Çin Zodyağı Burcunuz : Tavşan");
+                break;
+            case 8:
+                System.out.println("Çin Zodyağı Burcunuz : Ejderha");
+                break;
+            case 9:
+                System.out.println("Çin Zodyağı Burcunuz : Yılan");
+                break;
+            case 10:
+                System.out.println("Çin Zodyağı Burcunuz : At");
+                break;
+            case 11:
+                System.out.println("Çin Zodyağı Burcunuz : Koyun");
+                break;
+        }
+
+    }
+}
