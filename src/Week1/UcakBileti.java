bcff482277a82001473ec096546e280c50317cc0
emrecimen20 <emrecimen48@outlook.com>
2024-04-28 21:32:22 +0845

    Uçak bileti progframı güncellendi.

--- src/Week1/UcakBileti.java
+++ src/Week1/UcakBileti.java
@@ -1,65 +1,50 @@
+/* Uçak bileti fiyatı hesaplama. Kullanıcıdan mesafe, yaş ve yolculuk tipi girdileri
+alınarak ücret hesabı yapılacak. */
+
 package Week1;
 import java.util.Scanner;
 
 public class UcakBileti {
     public static void main(String[] args) {
-        int distance,age,tripType;
-        double price=0.10,normalPrice,yasIndirimi,indirimliTutar,gidisDonus,toplamTutar;
-        Scanner input=new Scanner(System.in);
+//Değişkenleri tanımlayalım
+        int mesafe, yas, yolculukTipi;
+        double birimFiyat = 0.10, toplamFiyat;
 
-        System.out.print("Mesafeyi km cinsinden giriniz :");
-        distance=input.nextInt();
-        System.out.print("Yaşınızı giriniz :");
-        age=input.nextInt();
-        System.out.print("Yolculuk tipini giriniz :");
-        tripType=input.nextInt();
+//Kullanıcıdan giriş değerlerini alalım.
+        Scanner input = new Scanner(System.in);
+        System.out.print("Mesafeyi giriniz (km)..: ");
+        mesafe = input.nextInt();
+        System.out.print("Yaşı giriniz: ");
+        yas = input.nextInt();
+        System.out.print("Yolculuk tipini giriniz (1 => Tek Yön , 2 => Gidiş Dönüş ):");
+        yolculukTipi = input.nextInt();
 
-        normalPrice=(distance*price);
+        // Veri doğruluğunu kontrol edelim.
+        if ((yolculukTipi != 1 && yolculukTipi != 2) || yas <= 0 || mesafe <= 0) {
+            System.out.println("Hatalı Veri Girdiniz !");
 
-        if (distance>0){
-            if((age<12) && (tripType==1)){
-                yasIndirimi=(normalPrice*50)/100;
-                indirimliTutar=normalPrice-yasIndirimi;
-                gidisDonus=(indirimliTutar*20)/100;
-                toplamTutar=(indirimliTutar-gidisDonus)*2;
-                System.out.print("Toplam Tutar :" + toplamTutar);
-            } else if ((age<12)&&(tripType==2)) {
-                yasIndirimi=(normalPrice*50)/100;
-                indirimliTutar=normalPrice-yasIndirimi;
-                System.out.println("Toplam Tutar :" + indirimliTutar);
-            } else if ((age>12) && (age<24) && (tripType==1)) {
-                yasIndirimi=(normalPrice*10)/100;
-                indirimliTutar=normalPrice-yasIndirimi;
-                gidisDonus=(indirimliTutar*20)/100;
-                toplamTutar=(indirimliTutar-gidisDonus)*2;
-                System.out.println("Toplam Tutar :" + toplamTutar);
-            } else if((age>12) && (age<24) && (tripType==2)) {
-                yasIndirimi=(normalPrice*10)/100;
-                indirimliTutar=normalPrice-yasIndirimi;
-                System.out.println("Toplam Tutar :" + indirimliTutar);
-            } else if ((age>24)&&(age<65)&&(tripType==1)) {
-                gidisDonus=(normalPrice*20)/100;
-                toplamTutar=(normalPrice-gidisDonus)*2;
-                System.out.println("Toplam tutar :" + toplamTutar);
-            } else if ((age>24)&&(age<65)&&(tripType==2)) {
-                System.out.println("Toplam Tutar :" + normalPrice);
-            } else if ((age>65) && tripType==1) {
-                yasIndirimi=(normalPrice*30)/100;
-                indirimliTutar=normalPrice-yasIndirimi;
-                gidisDonus=(indirimliTutar*20)/100;
-                toplamTutar=(indirimliTutar-gidisDonus)*2;
-                System.out.print("Toplam Tutar :" + toplamTutar);
-            } else if ((age>65) && (tripType==2)) {
-                yasIndirimi=(normalPrice*30)/100;
-                indirimliTutar=normalPrice-yasIndirimi;
-                System.out.println("Toplam Tutar :" + indirimliTutar);
-            }else{
-                System.out.println("Hatalı veri Girdiniz!");
-            }
-        }else {
-            System.out.println("Hatalı Mesafe Girişi");
+        } else {
+            toplamFiyat = mesafe * birimFiyat;
+
+            // İndirimleri uygulayalım
+            // 12 yaşından küçükse %50 indirim
+            if (yas < 12) {
+                toplamFiyat = toplamFiyat * 0.50;
+
+                // 12-24 yaş arası ise %10 indirim
+            } else if (yas >= 12 && yas <= 24) {
+                toplamFiyat = toplamFiyat * 0.90;
+
+                // 65 yaşından büyükse %30 indirim
+            } else if (yas >= 65) {
+                toplamFiyat = toplamFiyat * 0.70;}
+
+            // Gidiş-dönüş ise %20 indirim
+            if (yolculukTipi == 2) {
+                toplamFiyat = toplamFiyat * 0.80 * 2;}
+
+            // Sonuçları ekrana yazdır
+            System.out.printf("Toplam Tutar: " + toplamFiyat + "TL");
         }
-
-
     }
 }
