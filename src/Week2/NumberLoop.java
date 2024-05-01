7559ff660e76500006ce545d1649cfc2006c799a
emrecimen20 <emrecimen48@outlook.com>
2024-05-01 13:24:58 +0845

    Desene göre Metot programı

--- src/Week2/NumberLoop.java
+++ src/Week2/NumberLoop.java
@@ -1,31 +1,44 @@
-package Week2;
+/*Java dilinde kullanıcıdan alınan n değerine göre aşağıdaki kurala uyan döngü kullanmadan bir "Recursive" metot yazın.
 
+Kural : Girilen sayı 0 veya negatif olduğu yere kadar girilen sayıdan 5 rakamını çıkarın. Her çıkarma işlemi sırasında ekrana
+son değeri yazdırın. Sayı negatif veya 0 olduktan sonra tekrar 5 ekleyin. Yine her ekleme işleminde sayının son değerini ekrana yazdırın.
+
+N Sayısı : 16
+Çıktısı : 16 11 6 1 -4 1 6 11 16
+
+*/
+
+package Week2;
 import java.util.Scanner;
 
 public class NumberLoop {
 
     public static void loop(int number) {
-        System.out.print(number+ " ");
-       if(number>0){
-           number-=5;
-           loop(number);
 
-       } else if (number<=0) {
-           System.out.print(number);
-           number+=5;
-           loop(number);
-       }
+
+        //Bu kpşulu sağlayarak ve recursive metot kullanarak deseni elde ettik.
+       if(number<=0){
+           System.out.print(number+ " ");
+           return;
+
+       }else if(number>0){
+           System.out.print(number + " ");
+           loop(number-5);
+           System.out.print(number + " ");
+        }
+
 
 
     }
 
     public static void main(String[] args) {
-
+        //Kullanıcın gireceği sayıyı değişkene atadık ve Scanner kullanarak girişi sağladım.
         int number;
         Scanner input=new Scanner(System.in);
         System.out.print("Sayı Giriniz :");
         number=input.nextInt();
 
+        //Döngü metodunu çağırdım.
        loop(number);
     }
 }
