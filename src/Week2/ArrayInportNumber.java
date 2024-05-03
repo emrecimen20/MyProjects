33e19f1fba11a4fba76cda8b68290996a2981b64
emrecimen20 <emrecimen48@outlook.com>
2024-05-03 22:09:02 +0300

    Dizideki elemanların girilen sayıdan küçük en yakın sayıyı ve en büyük en yakın sayıyı bulan programı eklendi.

---
+++ src/Week2/ArrayInportNumber.java
@@ -0,0 +1,32 @@
+package Week2;
+import java.util.Scanner;
+
+public class ArrayInportNumber {
+    public static void main(String[] args) {
+        int n; boolean sayiBulundu;
+        Scanner input=new Scanner(System.in);
+
+        int[] arr={15,12,788,1,-1,-778,2,0};
+        System.out.print("Girilen sayı :");
+        n=input.nextInt();
+
+        int enKucukYakinSayi = Integer.MIN_VALUE;
+        int enBuyukYakinSayi = Integer.MAX_VALUE;
+
+        for (int i : arr) {
+            if (i == n) {
+                sayiBulundu = true;
+                break;
+                }
+            if (i < n && i > enKucukYakinSayi) {
+                enKucukYakinSayi = i;
+            }
+            if (i > n && i < enBuyukYakinSayi) {
+                enBuyukYakinSayi = i;
+            }
+        }
+
+        System.out.println("Girilen sayıdan küçük, en yakın sayı :" + enKucukYakinSayi);
+        System.out.println("Girilen sayıdan büyük, en yakın sayı :" + enBuyukYakinSayi);
+    }
+}
