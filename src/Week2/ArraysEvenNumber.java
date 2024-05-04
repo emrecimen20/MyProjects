584e31536a4d1a61c6ba109106cb8550c0d733b7
emrecimen20 <emrecimen48@outlook.com>
2024-05-04 17:49:41 +0300

    Dizide tekrar eden çift sayıları bulan program.

--- src/Week2/ArraysEvenNumber.java
+++ src/Week2/ArraysEvenNumber.java
@@ -1,7 +1,9 @@
-package Week2;
 
+//Dizideki tekrar eden çift sayıları bulan program.
+package Week2;
 public class ArraysEvenNumber {
 
+    //Bu metodu yeni diziye, tekrar eden sayılardan sadece birini koymak için yazdık.
     static boolean isFind(int[] arr,int value){
         for(int i: arr){
             if(value==i){
@@ -13,8 +15,8 @@ public class ArraysEvenNumber {
 
     public static void main(String[] args) {
         int[] list={4,7,4,4,2,8,10,21,10,33,8};
-        int[] duplicate=new int[list.length];
-        int startIndex=0;
+        int[] duplicate=new int[list.length]; //Tekrar eden çift sayıları ekleyeceğimiz yeni dizi oluşturdum.
+        int startIndex=0; //Yeni dizimin index numarasını bu değişken olarak belirledim.
         for(int i=0; i<list.length;i++){
             for(int j=0;j<list.length;j++){
                 if((i!=j) && (list[i]==list[j]) && (list[i]%2==0)){
@@ -25,7 +27,7 @@ public class ArraysEvenNumber {
                 }
             }
         }
-
+        //Yeni dizimi foreach ile dönerek sadece tekrar eden sayıları ekrana bastıdım.
         for(int i:duplicate){
             if(i!=0){
                 System.out.println(i);
