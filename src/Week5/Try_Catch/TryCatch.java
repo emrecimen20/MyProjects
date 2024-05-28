bce4665c26842f70ab44ce996ec88a686d595828
emrecimen20 <emrecimen48@outlook.com>
2024-05-29 00:01:29 +0300

    SQL homework8 push.

--- src/Week5/Try_Catch/TryCatch.java
+++ src/Week5/Try_Catch/TryCatch.java
@@ -1,7 +1,7 @@
 /*Soru: 10 elemanlı tek boyutlu bir dizi içerisinde kullanıcıdan alınan indeksteki elemanı döndüren bir Java metodu yazın.
 Eğer belirtilen indeks dizi boyutunun dışındaysa, metot bir hata mesajı döndürmelidir.Dizinin elmanlarını manuel olarak tanımlayın.*/
 
-package Week5;
+package Week5.Try_Catch;
 import java.util.Scanner;
 
 public class TryCatch {
