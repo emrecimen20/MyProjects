575ae4f3c9cc83745bbdd7ae47e899b251e13304
emrecimen20 <emrecimen48@outlook.com>
2024-06-01 22:51:08 +0300

    Patika Store add

---
+++ src/Week5/Patika_Store/Brand.java
@@ -0,0 +1,24 @@
+package Week5.Patika_Store;
+
+public class Brand {
+    private final int id;
+    private final String name;
+
+    public int getId () {
+        return id;
+    }
+
+    public String getName () {
+        return name;
+    }
+
+    public Brand (int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return id + ". " + name;
+    }
+}
---
+++ src/Week5/Patika_Store/Main.java
@@ -0,0 +1,9 @@
+package Week5.Patika_Store;
+
+public class Main {
+    public static void main (String[] args) {
+        Store store = new Store();
+        store.stockInventory();
+        store.run();
+    }
+}
