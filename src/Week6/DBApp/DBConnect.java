ffbed38267fc8a2943bc7167806b1223f92512e0
emrecimen20 <emrecimen48@outlook.com>
2024-06-02 16:32:33 +0300

    DB app add

---
+++ src/Week6/DBApp/DBConnect.java
@@ -0,0 +1,28 @@
+package Week6.DBApp;
+
+import java.sql.*;
+
+public class DBConnect {
+
+    public static final String DB_URL = "jdbc:postgresql://localhost:5432/employee";
+    public static final String DB_USER = "postgres";
+    public static final String DB_PASSWORD = "130440";
+
+    public static void main(String[] args) {
+        Connection connect = null;
+        String sql = "SELECT * FROM employees ";
+        try {
+            connect = DriverManager.getConnection(DB_URL,DB_USER,DB_PASSWORD);
+            Statement st = connect.createStatement();
+           ResultSet data = st.executeQuery(sql);
+           while(data.next()){
+               System.out.println("ID : " + data.getInt("id"));
+               System.out.println("Name : " + data.getString("name"));
+               System.out.println("Position : " + data.getString("position"));
+               System.out.println("Salary : " + data.getInt("salary"));
+               System.out.println("################");
+           }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());        }
+    }
+}
